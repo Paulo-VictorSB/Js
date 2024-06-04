c52254759c080a6296ab09a0f165c3543b75de35
Paulo Victor dos Santos Barbosa <paulo.vdbarbosa@gmail.com>
2024-06-04 11:37:11 -0300

    Adicionando archives

---
+++ Aulas/Condicionais/Exercícios Propostos/Exercicio02/assets/js/app.js
@@ -0,0 +1,58 @@
+function cResultado() {
+    let estados = document.getElementById('estados').value;
+    if (estados == 'acre') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/acre.png)'
+    } else if (estados == 'alagoas') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/alagoas.png)'
+    } else if (estados == 'amapa') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/amapa.png)'
+    } else if (estados == 'amazonas') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/amazonas.png)'
+    } else if (estados == 'bahia') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/bahia.png)'
+    } else if (estados == 'ceara') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/ceara.png)'
+    } else if (estados == 'distritoFederal') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/distritoFederal.png)'
+    } else if (estados == 'espiritoSanto') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/espiritoSanto.png)'
+    } else if (estados == 'goias') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/goias.png)'
+    } else if (estados == 'maranhao') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/maranhao.png)'
+    } else if (estados == 'matoGrosso') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/matoGrosso.png)'
+    } else if (estados == 'matoGrossoDoSul') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/matoGrossoDoSul.png)'
+    } else if (estados == 'minasGerais') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/minasGerais.png)'
+    } else if (estados == 'para') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/para.png)'
+    } else if (estados == 'paraiba') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/paraiba.png)'
+    } else if (estados == 'parana') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/parana.png)'
+    } else if (estados == 'pernambuco') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/pernambuco.png)'
+    } else if (estados == 'piaui') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/piaui.png)'
+    } else if (estados == 'rioDeJaneiro') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/rioDeJaneiro.png)'
+    } else if (estados == 'rioGrandeDoNorte') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/rioGrandeDoNorte.png)'
+    } else if (estados == 'rioGrandeDoSul') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/rioGrandeDoSul.png)'
+    } else if (estados == 'rondonia') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/rondonia.png)'
+    } else if (estados == 'roraima') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/roraima.png)'
+    } else if (estados == 'santaCatarina') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/santaCatarina.png)'
+    } else if (estados == 'saoPaulo') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/saoPaulo.png)'
+    } else if (estados == 'sergipe') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/sergipe.png)'
+    } else if (estados == 'tocantins') {
+        document.querySelector('div#imagemEstado').style.background = 'url(assets/images/tocantins.png)'
+    }
+}
---
+++ Aulas/Condicionais/JS/condicoes-simples.js
@@ -0,0 +1,10 @@
+let vel = 60.8
+console.log(`Sua velocidade é de ${vel}Km/h`)
+if (vel < 50) {
+    console.log('Você está dirigindo perfeitamente, continue assim')
+} else if (vel == 50) {
+    console.log('Dirija com cuidado! Você está no limite da via.')
+} else {
+    console.log('Você passou dos limites da via, MULTADO!')
+}
+console.log('Use sempre cinto de segurança e fique atento!')
